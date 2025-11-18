05c4aa79892be5189d7da112bb55eee00bb400ac
shrikantsupekar <supekarshrikant1@gmail.com>
2025-11-19 01:59:36 +0530

    updated API URL

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -10,7 +10,7 @@ const config: {
     API_URL: 'http://localhost:3000',
   },
   production: {
-    API_URL: 'http://localhost:3000',
+    API_URL: 'https://calculator-backend-gray.vercel.app',
   },
 }
 
